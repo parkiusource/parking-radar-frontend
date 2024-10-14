2a108f36eb0905fcdb9d0e0c1eda4503a566f60c
MARodriguez123 <miguelangel08_08@hotmail.com>
2024-10-13 23:18:01 -0500

    Visual improvements in the Front End

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from '../assets/smart-parking-logo.png';
+import logo from '../assets/smart-parking-logo4.png';
 
 const Header = () => (
   <header className="app-header">
-    <img src={logo} alt="Logo" className="app-logo" />
+    <img src={logo} alt="Logo" style={{ width: '70px', height: 'auto' }} className="app-logo" />
     <h1>Smart Parking Radar</h1>
   </header>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,8 @@ import Footer from '../components/Footer';
 import MapPage from '../pages/MapPage';
 import HomePage from '../pages/HomePage';
 import RegistroAdmin from '../pages/RegisterAdmin';
+import PQRSForm from '../pages/PQRSForm';
+import Features from '../pages/Features';
 
 const AppRoutes = () => (
   <>
@@ -16,6 +18,8 @@ const AppRoutes = () => (
         <Route path="/" element={<HomePage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/register-admin" element={<RegistroAdmin/>} />
+        <Route path="/features" element={<Features/>} />
+        <Route path="/pqrs" element={<PQRSForm/>} />
       </Routes>
     </div>
     <Footer />
