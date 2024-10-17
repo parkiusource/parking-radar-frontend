e8902aa1af6571aeee6085847df0f147dcbcd709
Wilson Camilo Leon Payanene <leon971010@gmail.com>
2024-10-17 16:51:45 -0500

    Sending ping from web socket service-client

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -1,15 +1,22 @@
 let socket;
+let pingInterval;
 
 export const connectWebSocket = (url, onMessage) => {
   socket = new WebSocket(url);
 
   socket.onopen = () => {
     console.log('Connected to WebSocket server');
+
+    pingInterval = setInterval(() => {
+      console.log('Sending ping');
+      socket.send(JSON.stringify({ type: 'ping' }));
+    }, 30000);
   };
 
   socket.onmessage = (event) => {
     try {
       const data = JSON.parse(event.data);
+      console.log('Message from server:', data);
       onMessage(data);
     } catch (error) {
       console.error('Error parsing WebSocket message:', error);
@@ -22,11 +29,19 @@ export const connectWebSocket = (url, onMessage) => {
 
   socket.onclose = () => {
     console.log('WebSocket connection closed');
+
+    if (pingInterval) {
+      clearInterval(pingInterval);
+    }
   };
 };
 
 export const closeWebSocket = () => {
   if (socket) {
     socket.close();
+  }
+
+  if (pingInterval) {
+    clearInterval(pingInterval);
   }
 };
