499aae9259d672aee25ec164612b072ff826b6fe
CamiloLeonP <leon971010@gmail.com>
2024-10-23 00:01:38 -0500

    set new var env  (#24)

--- src/services/ParkingService.js
+++ src/services/ParkingService.js
@@ -1,12 +1,11 @@
 // src/services/ParkingService.js
 import axios from 'axios';
 
-const API_URL = 'https://parking-radar-61e65e5cb889.herokuapp.com';
+const API_BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
 
-// Función para obtener los datos de los parqueaderos desde el backend
 export const getParkingSpots = async () => {
   try {
-    const response = await axios.get(`${API_URL}/parking-lots/`);
+    const response = await axios.get(`${API_BACKEND_URL}/parking-lots/`);
     return response.data;
   } catch (error) {
     console.error('Error getting the parking spots:', error);
